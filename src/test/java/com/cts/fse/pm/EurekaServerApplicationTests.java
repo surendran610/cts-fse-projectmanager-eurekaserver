0bd4da0fdff02c1d4376d84da2e601bf9abfb20c
Suren <ctsjavauser113@iiht.tech>
2020-06-19 16:20:33 +0530

    initial commit

---
+++ src/test/java/com/cts/fse/pm/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.fse.pm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
